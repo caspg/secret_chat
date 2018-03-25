c49f32e8ffc2b5291d1528a973da13d2f013a0fa
Kacper Golinski <kacper.golinski@gmail.com>
2018-03-25 21:40:53 +0200

    create current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    ctx = Users::FindBySecretId.call(user_secret_id: params[:user_secret_id])
+    ctx.user
+  end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,18 +2,13 @@ class RoomsController < ApplicationController
   NUMBER_OF_VISIBLE_MESSAGES = 50
 
   def show
-    #
-    # TODO: should there be just single UseCase that
-    # will return all needed objects (room, user, guests)?
-    #
     @room = find_room
-    @user = find_user
 
-    return redirect_to root_path if !@room || !@user
+    return redirect_to root_path if !@room || !current_user
 
     @guest = User.new(room_id: @room.id)
     @guests = @room.guests
-    @user_is_room_admin = @room.owner == @user
+    @user_is_room_admin = @room.owner == current_user
 
     # TODO: create use_case
     @messages = @room.messages.includes(:user).last(NUMBER_OF_VISIBLE_MESSAGES)
@@ -25,10 +20,5 @@ class RoomsController < ApplicationController
   def find_room
     ctx = Rooms::FindBySecretId.call(room_secret_id: params[:room_secret_id])
     ctx.room
-  end
-
-  def find_user
-    ctx = Users::FindBySecretId.call(user_secret_id: params[:user_secret_id])
-    ctx.user
   end
 end
